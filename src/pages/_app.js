55d6918a39a8262736cfbe37247e4407b0e2aa2c
Vitor Butkus <vitorbutkus@gmail.com>
2023-02-18 19:04:23 -0300

    change Head, add og

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,6 +16,22 @@ const App = ({ Component, pageProps }) => {
           name='description'
           content='Personal notes on technology from a humanized point of view'
         />
+
+        <meta property='og:title' content='notes on technology' />
+        <meta name='twitter:card' content='summary'></meta>
+        <meta name='twitter:creator' content='@jaguattt'></meta>
+        <meta
+          property='og:image'
+          content='https://thumbs.dreamstime.com/b/touchscreen-technology-icon-145272266.jpg'
+        />
+        <meta
+          property='og:url'
+          content='https://blog-nextjs-4wgijzy5v-vitorbutkus.vercel.app/'
+        />
+        <meta
+          property='og:description'
+          content='Personal notes on technology from a humanized point of view'
+        />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
