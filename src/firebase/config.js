4e19adc55b66a52aca9d0f51d2e239f67e3e3006
Vitor Butkus <vitorbutkus@gmail.com>
2023-02-18 18:00:22 -0300

    update env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,6 +8,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
 };
 
 const app = firebase.initializeApp(firebaseConfig);
