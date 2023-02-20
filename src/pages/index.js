2cc60e20502e821d36a87e6246feb76581eb4944
Vitor Butkus <vitorbutkus@gmail.com>
2023-02-20 15:22:41 -0300

    add next 3 posts & all posts button on index.js, add showPosts state on index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,20 @@ import Head from 'next/head';
 import { projectFirestore } from '@/firebase/config';
 import PostItem from '@/components/PostItem';
 import Header from '@/components/Header';
+import { TfiMore } from 'react-icons/tfi';
+import { useState } from 'react';
+import Link from 'next/link';
 
 export default function Home(props) {
   const data = props.latestPosts;
+  const [showPosts, setShowPosts] = useState(3);
+  const handleMore3 = () => {
+    if (showPosts <= 6) {
+      setShowPosts(showPosts + 3);
+    } else {
+      return;
+    }
+  };
 
   return (
     <>
@@ -13,15 +24,35 @@ export default function Home(props) {
       <div className='w-full'>
         <div className='max-w-[1240px] mx-auto'>
           <div className='h-full w-full'>
-            {data.map((post) => (
-              <PostItem
-                title={post.title}
-                text={post.text}
-                key={post.id}
-                date={post.date}
-                id={post.id}
-              />
-            ))}
+            {data.map(
+              (post, i) =>
+                i < showPosts && (
+                  <PostItem
+                    title={post.title}
+                    text={post.text}
+                    key={post.id}
+                    date={post.date}
+                    id={post.id}
+                  />
+                )
+            )}
+          </div>
+
+          <div className='pb-20 px-2 flex justify-between'>
+            {showPosts <= 6 && (
+              <div
+                className='bg-slate-300 py-2 px-4 rounded-md cursor-pointer'
+                onClick={handleMore3}
+              >
+                <p>next 3 posts</p>
+              </div>
+            )}
+
+            <Link href={`/posts#${showPosts}`}>
+              <div className='bg-slate-300 py-2 px-4 rounded-md cursor-pointer'>
+                <p>all posts</p>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
@@ -32,7 +63,7 @@ export default function Home(props) {
 export async function getServerSideProps() {
   const ref = projectFirestore.collection('posts');
 
-  const data = await ref.orderBy('date', 'desc').limit(3).get();
+  const data = await ref.orderBy('date', 'desc').limit(9).get();
   const results = data.docs.map((post, i) => ({
     id: post.id,
     ...post.data(),
